37e2030bcfcd16758fa09318866c783fae9230f7
Ignacio Senra Oka <isenras@hotmail.com>
2023-12-15 19:03:26 -0300

    Namespace fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     devise_for :users
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "up" => "rails/health#show", as: :rails_health_check
   root "posts#index"
